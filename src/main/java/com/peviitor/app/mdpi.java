dbba8e99319927809993eaedc017a340809a181e
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-06-27 02:14:18 +0300

    create mdpi scraper

---
+++ src/main/java/com/peviitor/app/mdpi.java
@@ -0,0 +1,71 @@
+package com.peviitor.app;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+
+public class mdpi {
+    public static JSONObject company = new JSONObject().put("company", "MDPI");
+    public static String country = "Romania";
+    public static void main(String[] args) {
+        Document document = Jsoup.parse("");
+        try {
+            document = Jsoup.connect("https://careers.ro.mdpi.com/jobs?query=").get();
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+
+        ArrayList<JSONObject> jobs = new ArrayList<JSONObject>();
+        
+        Elements jobElements = document.getElementsByClass("z-career-job-card-image");
+        for (Element element : jobElements) {
+            JSONObject job = new JSONObject();
+
+            job.put("id", UUID.randomUUID().toString());
+            job.put("job_title", element.getElementsByClass("text-block-base-link").text());
+            job.put("job_link", element.getElementsByTag("a")
+                                            .attr("href"));
+            job.put("company", company.getString("company"));
+            job.put("country", country);
+            job.put("city", element.getElementsByClass("mt-1 text-md")
+                                        .first()   
+                                        .getElementsByTag("span")
+                                        .last()
+                                        .text());
+
+            jobs.add(job);
+        }
+
+        System.out.println(JSONObject.valueToString(jobs));
+
+        try {
+            // // Clean Data from API
+            utils.post("https://api.peviitor.ro/v4/clean/", JSONObject.valueToString(company), new JSONObject[] {
+                new JSONObject().put("Content-Type", "application/x-www-form-urlencoded").put("apikey", "182b157-bb68-e3c5-5146-5f27dcd7a4c8")
+            });
+
+            // // Add Data to API
+            utils.post("https://api.peviitor.ro/v4/update/", JSONObject.valueToString(jobs), new JSONObject[] {
+                new JSONObject().put("Content-Type", "application/json").put("apikey", "182b157-bb68-e3c5-5146-5f27dcd7a4c8")
+            });
+
+            // Add logo
+            String logo = "https://images.teamtailor-cdn.com/images/s3/teamtailor-production/logotype-v3/image_uploads/9a39cb44-b1c9-4c5e-8282-9256dd0e070d/original.png";
+            ArrayList<JSONObject> logoData = new ArrayList<JSONObject>();
+            logoData.add(new JSONObject().put("id", company.getString("company")).put("logo", logo));
+
+            utils.post("https://api.peviitor.ro/v1/logo/add/", JSONObject.valueToString(logoData), new JSONObject[] {
+                new JSONObject().put("Content-Type", "application/json"),
+            });
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
---
+++ src/main/java/com/peviitor/app/utils.java
@@ -0,0 +1,57 @@
+package com.peviitor.app;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.json.JSONObject;
+
+public class utils {
+    public static String post(String urlString, String data, JSONObject... header) throws Exception {
+        URL url = new URL(urlString);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+        String requestBody = data;
+
+        for (JSONObject h : header) {
+            for (String key : h.keySet()) {
+                con.setRequestProperty(key, h.getString(key));
+                if (h.getString(key).equals("application/x-www-form-urlencoded")) {
+                    Map<String, String> dataHash = new HashMap<>();
+
+                    JSONObject dataJSON = new JSONObject(data);
+                    for (String k : dataJSON.keySet()) {
+                        dataHash.put(k, dataJSON.getString(k));
+                    }
+                    requestBody = dataHash.keySet().stream()
+                            .map(key2 -> key2 + "=" + dataHash.get(key2))
+                            .collect(Collectors.joining("&"));
+                }
+            }
+        }
+
+        con.setRequestMethod("POST");
+        con.setDoOutput(true);
+        con.getOutputStream().write(requestBody.getBytes(StandardCharsets.UTF_8));
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+
+        in.close();
+        con.disconnect();
+
+        return null;
+    }
+}
